77ccdfb5bced804df2297eba4dca133a32704a02
crookedconverse <matthewneeper10@gmail.com>
2019-02-06 16:18:20 -0600

    Updated index.html
    Removed Code that was unnecessary and frmo older version of website. Reworked link section and changed order links appear.

---
+++ HR/js/drop_down.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+  var x = document.getElementById("navbarnav");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
