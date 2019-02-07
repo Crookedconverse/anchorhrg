6268bedb5f06a4b435e3ccbca6a09abfbb41bbab
Matthew Neeper <matthewneeper10@gmail.com>
2019-02-06 20:27:57 -0600

    HR ReadyPay Mobile Update
    I have updated the mobile version of HR ReadyPay. Specifically screens at or less than 320px wide.

---
+++ HR/js/myrotate.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+  var x = document.getElementByClass("myFunction");
+  if (x.style.transform === "none") {
+    x.style.tranform = "(90deg)";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
